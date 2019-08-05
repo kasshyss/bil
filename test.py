492847a075fb03949c686002a96f684443ba06b5
Meriadoc <pierre@bourgain.org>
2019-08-05 16:48:05 +0200

    minor update

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import lib.m_logger as log
 import lib.m_conf as conf
 import lib.m_request as request
 import lib.m_file as f
+import html_parser as hp
 import pandas as pd
 import os
 
@@ -13,14 +14,15 @@ section = 'QUERIES'
 print(log.get_logger_level(''))
 c = conf.get_section_conf(cf, path +'/conf', section)
 print(c['guichet_lu'])
-#d = request.request_html(str(c['guichet_lu']+'1'))
+d = request.request_html(str(c['guichet_lu']+'1'))
+dico = hp.code_nace(d)
+
+print dico
 #print(d)
-a = f.csv_to_df(path + '/save', 'test.csv')
-print(a.shape[1])
-
-s = f.df_to_csv(path + '/save', 'test_out.csv', a)
-print(s)
-
-s = f.append_df_to_csv(path + '/save', 'test_out.csv', a)
-print(s)
+#a = f.csv_to_df(path + '/save', 'test.csv')
+#print(a.shape[1])
+#s = f.df_to_csv(path + '/save', 'test_out.csv', a)
+#print(s)
+#s = f.append_df_to_csv(path + '/save', 'test_out.csv', a)
+#print(s)
 print('fin')
